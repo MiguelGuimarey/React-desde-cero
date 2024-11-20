56ee53e6d88ecbab88eefcd689db4d52c5bdd655
Miguel Guimarey Lesmes <MiguelGuimareyLesmes96@outlook.es>
2024-11-20 21:17:13 +0100

    Primer Inicio de Aplicación React de Clon De twitter (momento 1:13:46)

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import {TwitterFollowCard} from './TwitterFollowCard.jsx'
+
+export function App () {
+    return(
+        <>
+            <TwitterFollowCard userName="User1" name="Usuario 1"/>
+            <TwitterFollowCard userName="User2" name="Usuario 2"/>
+        </>
+    )
+}
