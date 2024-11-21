5716c4690a04d599a9cffbc37b232af83f09d8ec
Miguel Guimarey Lesmes <MiguelGuimareyLesmes96@outlook.es>
2024-11-21 17:08:45 +0100

    Adición de hover en el boton Seguir

--- projects/01-twitter-clon/src/TwitterFollowCard.jsx
+++ projects/01-twitter-clon/src/TwitterFollowCard.jsx
@@ -28,7 +28,8 @@ export function TwitterFollowCard({ userName = "unknown", name = "unknown" }) {
 
       <aside>
         <button className={buttonClassName} onClick={handleClick}>
-          {text}
+          <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de Seguir</span>
         </button>
       </aside>
     </article>
