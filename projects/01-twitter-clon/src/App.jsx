94ddfa5a751af9ad2e43612407b4d7a28c71768a
Miguel Guimarey Lesmes <MiguelGuimareyLesmes96@outlook.es>
2024-11-21 12:20:07 +0100

    Cambio de Nombre De La Carpeta

--- projects/01-twitter-clon/src/App.jsx
+++ projects/01-twitter-clon/src/App.jsx
@@ -3,9 +3,11 @@ import {TwitterFollowCard} from './TwitterFollowCard.jsx'
 
 export function App () {
     return(
-        <>
+        <section className='App'>
             <TwitterFollowCard userName="User1" name="Usuario 1"/>
             <TwitterFollowCard userName="User2" name="Usuario 2"/>
-        </>
+            <TwitterFollowCard userName="User3" name="Usuario 3"/>
+            <TwitterFollowCard userName="User4" name="Usuario 4"/>
+        </section>
     )
 }
