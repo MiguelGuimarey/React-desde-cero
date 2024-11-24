171c534422de96c147c576a9e6e56eaad9557f92
Miguel Guimarey Lesmes <MiguelGuimareyLesmes96@outlook.es>
2024-11-24 12:51:14 +0100

    Added Stored Turn and Board in session

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -11,9 +11,18 @@ import { WinnerModal } from "./components/WinnerModal";
 const board = Array(9).fill(null);
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(() => {
+    console.log("Inicializar estado del tablero");
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
 
-  const [turn, setTurn] = useState(TURNS.X);
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
+  });
 
   const [winner, setWinner] = useState(null);
 
@@ -21,6 +30,8 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
   };
 
   const updateBoard = (index) => {
@@ -33,7 +44,9 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
-
+    //guardar la partida
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
       confetti();
